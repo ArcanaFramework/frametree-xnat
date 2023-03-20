b382a912f22cd5430ce756138da75fd743f5c704
Tom Close <tom.g.close@gmail.com>
2023-03-20 11:38:34 +1100

    reworked tests config

--- arcana/xnat/data/tests/test_store.py
+++ arcana/xnat/data/tests/test_store.py
@@ -37,8 +37,8 @@ else:
 # logger.setLevel(logging.INFO)
 
 
-def test_populate_tree(xnat_dataset):
-    blueprint = xnat_dataset.__annotations__["blueprint"]
+def test_populate_tree(static_dataset: Dataset):
+    blueprint = static_dataset.__annotations__["blueprint"]
     for freq in Clinical:
         # For all non-zero bases in the row_frequency, multiply the dim lengths
         # together to get the combined number of rows expected for that
@@ -48,14 +48,14 @@ def test_populate_tree(xnat_dataset):
             (ln for ln, b in zip(blueprint.dim_lengths, freq) if b),
             1,
         )
-        assert len(xnat_dataset.rows(freq)) == num_rows, (
-            f"{freq} doesn't match {len(xnat_dataset.rows(freq))}" f" vs {num_rows}"
+        assert len(static_dataset.rows(freq)) == num_rows, (
+            f"{freq} doesn't match {len(static_dataset.rows(freq))}" f" vs {num_rows}"
         )
 
 
-def test_populate_row(xnat_dataset):
-    blueprint = xnat_dataset.__annotations__["blueprint"]
-    for row in xnat_dataset.rows("session"):
+def test_populate_row(static_dataset: Dataset):
+    blueprint = static_dataset.__annotations__["blueprint"]
+    for row in static_dataset.rows("session"):
         expected_entries = sorted(
             itertools.chain(
                 *(
@@ -67,19 +67,19 @@ def test_populate_row(xnat_dataset):
         assert sorted(e.path for e in row.entries) == expected_entries
 
 
-def test_get(xnat_dataset, caplog):
-    blueprint = xnat_dataset.__annotations__["blueprint"]
+def test_get(static_dataset: Dataset, caplog):
+    blueprint = static_dataset.__annotations__["blueprint"]
     expected_files = {}
     for scan_bp in blueprint.scans:
         for resource_bp in scan_bp.resources:
             if resource_bp.datatype is not None:
                 source_name = scan_bp.name + resource_bp.path
-                xnat_dataset.add_source(
+                static_dataset.add_source(
                     source_name, path=scan_bp.name, datatype=resource_bp.datatype
                 )
                 expected_files[source_name] = set(resource_bp.filenames)
     with caplog.at_level(logging.INFO, logger="arcana"):
-        for row in xnat_dataset.rows(Clinical.session):
+        for row in static_dataset.rows(Clinical.session):
             for source_name, files in expected_files.items():
                 try:
                     item = row[source_name]
@@ -89,7 +89,7 @@ def test_get(xnat_dataset, caplog):
                         / ".xnat4tests"
                         / "xnat_root"
                         / "archive"
-                        / xnat_dataset.id
+                        / static_dataset.id
                     )
                     archive_perms = get_perms(archive_dir)
                     current_user = os.getlogin()
@@ -102,15 +102,15 @@ def test_get(xnat_dataset, caplog):
                     p.name for p in item.fspaths if not p.name.endswith("catalog.xml")
                 )
                 assert item_files == sorted(Path(f).name for f in files)
-    method_str = "direct" if type(xnat_dataset.store) is XnatViaCS else "api"
+    method_str = "direct" if type(static_dataset.store) is XnatViaCS else "api"
     assert f"{method_str} access" in caplog.text.lower()
 
 
-def test_post(mutable_dataset: Dataset, source_data: Path, caplog):
-    blueprint = mutable_dataset.__annotations__["blueprint"]
+def test_post(dataset: Dataset, source_data: Path, caplog):
+    blueprint = dataset.__annotations__["blueprint"]
     all_checksums = {}
     for deriv_bp in blueprint.derivatives:
-        mutable_dataset.add_sink(
+        dataset.add_sink(
             name=deriv_bp.path,
             datatype=deriv_bp.datatype,
             row_frequency=deriv_bp.row_frequency,
@@ -126,18 +126,18 @@ def test_post(mutable_dataset: Dataset, source_data: Path, caplog):
         # else:
         #     relative_to = deriv_tmp_dir
         all_checksums[deriv_bp.path] = item.hash_files()
-        # Insert into first row of that row_frequency in xnat_dataset
-        row = next(iter(mutable_dataset.rows(deriv_bp.row_frequency)))
+        # Insert into first row of that row_frequency in dataset
+        row = next(iter(dataset.rows(deriv_bp.row_frequency)))
         with caplog.at_level(logging.INFO, logger="arcana"):
             row[deriv_bp.path] = item
-        method_str = "direct" if type(mutable_dataset.store) is XnatViaCS else "api"
+        method_str = "direct" if type(dataset.store) is XnatViaCS else "api"
         assert f"{method_str} access" in caplog.text.lower()
 
-    access_method = "cs" if type(mutable_dataset.store) is XnatViaCS else "api"
+    access_method = "cs" if type(dataset.store) is XnatViaCS else "api"
 
     def check_inserted():
         for deriv_bp in blueprint.derivatives:
-            row = next(iter(mutable_dataset.rows(deriv_bp.row_frequency)))
+            row = next(iter(dataset.rows(deriv_bp.row_frequency)))
             cell = row.cell(deriv_bp.path, allow_empty=False)
             item = cell.item
             assert isinstance(item, deriv_bp.datatype)
@@ -146,7 +146,7 @@ def test_post(mutable_dataset: Dataset, source_data: Path, caplog):
     if access_method == "api":
         check_inserted()  # Check cache
         # Check downloaded by deleting the cache dir
-        shutil.rmtree(mutable_dataset.store.cache_dir / "projects" / mutable_dataset.id)
+        shutil.rmtree(dataset.store.cache_dir / "projects" / dataset.id)
         check_inserted()
 
 
